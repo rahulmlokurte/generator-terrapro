4cd5b13bde9283716a134fd3d7049fbc63396d51
rahulmlokurte <rahul.m.lokurte@gmail.com>
2022-01-20 23:22:00 +0530

    Adding basic prompts and logging on console

--- generators/app/index.js
+++ generators/app/index.js
@@ -12,6 +12,26 @@ module.exports = class extends Generator {
                 'Welcome to the Terraform project generator'
             )
         )
+
+        this.answer = await this.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of your project?',
+                default: this.appname
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'What is the description of your project?',
+                default: 'A Terraform project'
+            }
+        ]);
+    }
+
+    writing() {
+        this.log("The Project name is: ", this.answer.name);
+        this.log("The description of project is: ", this.answer.description);
     }
 };
 
