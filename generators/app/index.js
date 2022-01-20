fb4a1c84e4775abbb1ddc0c76d8fad391bd06259
rahulmlokurte <rahul.m.lokurte@gmail.com>
2022-01-20 23:11:15 +0530

    Adding yosay

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,17 @@
 const Generator = require('yeoman-generator');
+yosay = require('yosay');
+
 module.exports = class extends Generator {
-    method1() {
-        this.log('method 1 just ran');
+    constructor(args, opts) {
+        super(args, opts);
+    }
+
+    async prompting() {
+        this.log(
+            yosay(
+                'Welcome to the Terraform project generator'
+            )
+        )
     }
 };
 
